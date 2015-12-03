1d96c5da6f28127825cd78705487173f2655992e
Andre Caetano <dezaodoido@gmail.com>
2015-12-03 17:50:21 -0200

    Minor changes :coffee:

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,20 +1,15 @@
-#!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-set -o errexit
+# #!/bin/bash
+# # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+# set -o errexit
 
-rm -rf dist
-mkdir dist
+# rm -rf dist
+# mkdir dist
 
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# build (CHANGE THIS)
-gulp build
-
-# deploy
-cd dist
 git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+# git add .
+# git commit -m "Deploy to Github Pages"
+# git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
